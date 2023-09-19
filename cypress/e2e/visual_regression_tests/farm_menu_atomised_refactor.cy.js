61da39c0f49b9c345aebc375cf16b8c7a91aaf99
KingsleyHep <khepburn@unitassoftware.com>
2023-09-19 15:55:48 +0100

    Naivgating Accounts Menu Buggy

--- cypress/e2e/visual_regression_tests/farm_menu_atomised_refactor.cy.js
+++ cypress/e2e/visual_regression_tests/farm_menu_atomised_refactor.cy.js
@@ -71,21 +71,85 @@ describe("visual regression tests for farm navigation bar options", () => {
     });
   });
 
-  it("takes a picture of the reports page", () => {
+  it("takes a picture of the farm reports page", () => {
     cy.get("#pageContainer").within(() => {
       cy.get('iframe[id="pageContent"]').then(function ($iframe) {
         const $body = $iframe.contents().find("body");
         cy.wrap($body).within(() => {
           cy.get("#Tab").contains("Reports").click();
           cy.waitForIframe("iframe#content");
-          cy.compareSnapshot("WC test reports page", 0.2);
+          cy.compareSnapshot("WC test farm reports page", 0.2);
         });
       });
     });
   });
 
-  //todo: add its for accounts
+  //todo: replace with reusable function - wait for egg grading
+  it("takes a picture of the accounts/egg grading page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Accounts");
+    });
+    cy.get("#pageContainer").within(() => {
+      cy.get('iframe[id="pageContent"]').then(function ($iframe) {
+        const $body = $iframe.contents().find("body");
+        cy.wrap($body).within(() => {
+          //!falling over at navigation bar despite being the same iframe structure as the farm menu bar
+          cy.get("#Tab").contains("Egg Grading").click();
+          cy.waitForIframe("iframe#content");
+          cy.compareSnapshot("WC test accounts EGG GRADING", 0.2);
+        });
+      });
+    });
+  });
 
-  //todo: add its for planning admin and logout
-  //end of describe scope
+  //transactions
+  it("takes a picture of the transactions page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Accounts");
+    });
+    //todo: replace with reusable function - wait for transactions
+  });
+
+  //payments
+  it("takes a picture of the payments page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Accounts");
+    });
+    //todo: replace with reusable function - wait for payments
+  });
+
+  //reports
+  it("takes a picture of the accounts reports page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Accounts");
+      //todo: replace with reusable function - wait for accounts reports
+    });
+  });
+
+  it("takes a picture of the planning page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Planning");
+    });
+    //todo: replace with reusable function - wait for planning
+    cy.wait(1500);
+    cy.compareSnapshot("WC test planning page", 0.2);
+  });
+
+  it("takes a picture of the admin page", () => {
+    cy.get("#DropDownContainer").within(() => {
+      cy.get("#menuIconsDD").select("Admin");
+    });
+    //todo: replace with reusable function - wait for admin
+    cy.wait(1500);
+    cy.compareSnapshot("WC test admin", 0.2);
+  });
+  //! logout is not taking picture of logout page
+  // it("takes a picture of the logout page", () => {
+  //   cy.get("#DropDownContainer").within(() => {
+  //     cy.wait(1500);
+  //     cy.compareSnapshot("WC test logout", 0.2);
+  //     cy.get("#menuIconsDD").select("Log Out");
+  //   });
+  //todo: replace with reusable function - wait for logout
 });
+//end of describe scope
